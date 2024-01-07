e8eb90e09e66b82ad3547b72f9656ea85a62aa21
Gokberk Gunes <prhi2@tutanota.com>
2024-01-07 18:39:09 +0300

    attach bottom patch applied
    This allows newly created windows to be added to the end of the window stack.
    Thus, the master window is never changed. Ultimately, this behavior
    change allows enables sub tasks to be done without disrupting the whole setup.

--- dwm.c
+++ dwm.c
@@ -155,6 +155,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interac
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachbottom(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void checkotherwm(void);
@@ -513,6 +514,15 @@ attach(Client *c)
 {
 	c->next = c->mon->clients;
 	c->mon->clients = c;
+}
+
+void
+attachbottom(Client *c)
+{
+	Client **tc;
+	c->next = NULL;
+	for (tc = &c->mon->clients; *tc; tc = &(*tc)->next);
+	*tc = c;
 }
 
 void
@@ -1437,7 +1447,8 @@ manage(Window w, XWindowAttributes *wa)
 		c->isfloating = c->oldstate = trans != None || c->isfixed;
 	if (c->isfloating)
 		XRaiseWindow(dpy, c->win);
-	attach(c);
+	//attach(c);
+	attachbottom(c);
 	attachstack(c);
 	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
 		(unsigned char *) &(c->win), 1);
@@ -1782,7 +1793,8 @@ sendmon(Client *c, Monitor *m)
 	arrange(c->mon);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-	attach(c);
+	//attach(c);
+	attachbottom(c);
 	attachstack(c);
 	arrange(m);
 	if (hadfocus) {
@@ -2399,7 +2411,8 @@ updategeom(void)
 				m->clients = c->next;
 				detachstack(c);
 				c->mon = mons;
-				attach(c);
+				//attach(c);
+				attachbottom(c);
 				attachstack(c);
 			}
 			if (m == selmon)
